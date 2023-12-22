0352d389773e22667033efd5e19be467c2df6cbc
ZongoForSpeed <13649539+ZongoForSpeed@users.noreply.github.com>
2023-12-22 22:37:33 +0100

    [AOC 2023] Day 21: Step Counter

---
+++ src/test/java/com/adventofcode/year2023/Day21Test.java
@@ -0,0 +1,86 @@
+package com.adventofcode.year2023;
+
+import com.adventofcode.point.Point2D;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+class Day21Test {
+
+    @Test
+    void inputExample() {
+        String input = """
+                ...........
+                .....###.#.
+                .###.##..#.
+                ..#.#...#..
+                ....#.#....
+                .##..S####.
+                .##..#...#.
+                .......##..
+                .##.#.####.
+                .##..##.##.
+                ...........""";
+
+        {
+            Scanner scanner = new Scanner(input);
+            List<Point2D> current = Day21.PartOne.stepCounter(scanner, 6);
+            Assertions.assertThat(current).hasSize(16);
+        }
+
+        // {
+        //     Scanner scanner = new Scanner(input);
+        //     Assertions.assertThat(PartTwo.infiniteStepCounter(scanner, 10)).isEqualTo(50);
+        // }
+
+        // {
+        //     Scanner scanner = new Scanner(input);
+        //     Assertions.assertThat(PartTwo.infiniteStepCounter(scanner, 50)).isEqualTo(1594);
+        // }
+
+        // {
+        //     Scanner scanner = new Scanner(input);
+        //     Assertions.assertThat(PartTwo.infiniteStepCounter(scanner, 100)).isEqualTo(6536);
+        // }
+
+        // {
+        //     Scanner scanner = new Scanner(input);
+        //     Assertions.assertThat(PartTwo.infiniteStepCounter(scanner, 500)).isEqualTo(167004);
+        // }
+
+        // {
+        //     Scanner scanner = new Scanner(input);
+        //     Assertions.assertThat(PartTwo.infiniteStepCounter(scanner, 1000)).isEqualTo(668697);
+        // }
+
+        // {
+        //     Scanner scanner = new Scanner(input);
+        //     Assertions.assertThat(PartTwo.infiniteStepCounter(scanner, 5000)).isEqualTo(16733044);
+        // }
+    }
+
+
+    @Test
+    void inputPartOne() throws IOException {
+        try (InputStream inputStream = Day21Test.class.getResourceAsStream("/2023/day/21/input");
+             Scanner scanner = new Scanner(Objects.requireNonNull(inputStream))) {
+            List<Point2D> current = Day21.PartOne.stepCounter(scanner, 64);
+            Assertions.assertThat(current).hasSize(3637);
+        }
+    }
+
+    @Test
+    void inputPartTwo() throws IOException {
+        try (InputStream inputStream = Day21Test.class.getResourceAsStream("/2023/day/21/input");
+             Scanner scanner = new Scanner(Objects.requireNonNull(inputStream))) {
+            long counter = Day21.PartTwo.infiniteStepCounter(scanner, 26501365);
+            Assertions.assertThat(counter).isEqualTo(601113643448699L);
+        }
+    }
+
+}
