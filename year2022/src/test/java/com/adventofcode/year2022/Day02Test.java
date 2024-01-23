48bdf1f077db9abbb44a5e2b970b5265bd5822e8
ZongoForSpeed <13649539+ZongoForSpeed@users.noreply.github.com>
2024-01-23 18:22:00 +0100

    [AOC 2022] Day 2: Rock Paper Scissors

---
+++ year2022/src/test/java/com/adventofcode/year2022/Day02Test.java
@@ -0,0 +1,52 @@
+package com.adventofcode.year2022;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Scanner;
+
+class Day02Test {
+
+    @Test
+    void inputExample() {
+        String input = """
+                A Y
+                B X
+                C Z""";
+
+        {
+            Scanner scanner = new Scanner(input);
+            int score = Day02.PartOne.findScore(scanner);
+            Assertions.assertThat(score).isEqualTo(15);
+        }
+
+        {
+            Scanner scanner = new Scanner(input);
+            int score = Day02.PartTwo.findScore(scanner);
+            Assertions.assertThat(score).isEqualTo(12);
+        }
+    }
+
+    @Test
+    void inputPartOne() throws IOException {
+        try (InputStream inputStream = Day02Test.class.getResourceAsStream("/2022/day/02/input");
+             Scanner scanner = new Scanner(Objects.requireNonNull(inputStream))) {
+            int score = Day02.PartOne.findScore(scanner);
+            Assertions.assertThat(score).isEqualTo(15691);
+        }
+    }
+
+    @Test
+    void inputPartTwo() throws IOException {
+        try (InputStream inputStream = Day02Test.class.getResourceAsStream("/2022/day/02/input");
+             Scanner scanner = new Scanner(Objects.requireNonNull(inputStream))) {
+            int score = Day02.PartTwo.findScore(scanner);
+            Assertions.assertThat(score).isEqualTo(12989);
+        }
+    }
+
+
+}
