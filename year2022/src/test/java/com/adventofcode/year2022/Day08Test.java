c864e2f7b5e93a5419f8a84a60d2c520ebb92e33
ZongoForSpeed <13649539+ZongoForSpeed@users.noreply.github.com>
2024-01-27 17:26:30 +0100

    [AOC 2022] Day 8: Treetop Tree House

---
+++ year2022/src/test/java/com/adventofcode/year2022/Day08Test.java
@@ -0,0 +1,52 @@
+package com.adventofcode.year2022;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Scanner;
+
+class Day08Test {
+
+    @Test
+    void inputExample() {
+        String input = """
+                30373
+                25512
+                65332
+                33549
+                35390""";
+
+        {
+            Scanner scanner = new Scanner(input);
+            long cardinality = Day08.PartOne.countVisibleTrees(scanner);
+            Assertions.assertThat(cardinality).isEqualTo(21);
+        }
+
+        {
+            Scanner scanner = new Scanner(input);
+            int maxScenicScore = Day08.PartTwo.maxScenicScore(scanner);
+            Assertions.assertThat(maxScenicScore).isEqualTo(8);
+        }
+    }
+
+    @Test
+    void inputPartOne() throws IOException {
+        try (InputStream inputStream = Day08Test.class.getResourceAsStream("/2022/day/08/input");
+             Scanner scanner = new Scanner(Objects.requireNonNull(inputStream))) {
+            long cardinality = Day08.PartOne.countVisibleTrees(scanner);
+            Assertions.assertThat(cardinality).isEqualTo(1820);
+        }
+    }
+
+    @Test
+    void inputPartTwo() throws IOException {
+        try (InputStream inputStream = Day08Test.class.getResourceAsStream("/2022/day/08/input");
+             Scanner scanner = new Scanner(Objects.requireNonNull(inputStream))) {
+            int maxScenicScore = Day08.PartTwo.maxScenicScore(scanner);
+            Assertions.assertThat(maxScenicScore).isEqualTo(385112);
+        }
+    }
+}
