9b2305b06951d631fc526b8d91f6729f65952f2d
ZongoForSpeed <13649539+ZongoForSpeed@users.noreply.github.com>
2024-01-29 22:24:25 +0100

    [AOC 2022] Day 9: Rope Bridge

---
+++ year2022/src/test/java/com/adventofcode/year2022/Day09Test.java
@@ -0,0 +1,75 @@
+package com.adventofcode.year2022;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Scanner;
+
+class Day09Test {
+
+    @Test
+    void inputExample1() {
+        String input = """
+                R 4
+                U 4
+                L 3
+                D 1
+                R 4
+                D 1
+                L 5
+                R 2""";
+
+        {
+            Scanner scanner = new Scanner(input);
+            int size = Day09.PartOne.countTailPositions(scanner);
+            Assertions.assertThat(size).isEqualTo(13);
+        }
+
+        {
+            Scanner scanner = new Scanner(input);
+            int size = Day09.PartTwo.countTailPositions(scanner);
+            Assertions.assertThat(size).isEqualTo(1);
+        }
+    }
+
+    @Test
+    void inputExample2() {
+        String input = """
+                R 5
+                U 8
+                L 8
+                D 3
+                R 17
+                D 10
+                L 25
+                U 20""";
+
+        {
+            Scanner scanner = new Scanner(input);
+            int size = Day09.PartTwo.countTailPositions(scanner);
+            Assertions.assertThat(size).isEqualTo(36);
+        }
+    }
+
+    @Test
+    void inputPartOne() throws IOException {
+        try (InputStream inputStream = Day09Test.class.getResourceAsStream("/2022/day/09/input");
+             Scanner scanner = new Scanner(Objects.requireNonNull(inputStream))) {
+            int size = Day09.PartOne.countTailPositions(scanner);
+            Assertions.assertThat(size).isEqualTo(5858);
+        }
+    }
+
+    @Test
+    void inputPartTwo() throws IOException {
+        try (InputStream inputStream = Day09Test.class.getResourceAsStream("/2022/day/09/input");
+             Scanner scanner = new Scanner(Objects.requireNonNull(inputStream))) {
+            int size = Day09.PartTwo.countTailPositions(scanner);
+            Assertions.assertThat(size).isEqualTo(2602);
+        }
+    }
+
+}
